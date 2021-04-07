806d4a1970d6d6477dd3818ed969fe79bd1131f9
mikaelnorrman <mikael.norrman@gmail.com>
2021-04-07 21:45:52 +0200

    first commit

---
+++ src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
@@ -0,0 +1,54 @@
+package com.vaadin.tutorial.crm.ui;
+
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
+import com.vaadin.flow.router.Route;
+import com.vaadin.tutorial.crm.backend.entity.Company;
+import com.vaadin.tutorial.crm.backend.entity.Contact;
+import com.vaadin.tutorial.crm.backend.service.ContactService;
+
+@Route("")
+public class MainView extends VerticalLayout {
+
+    private Grid<Contact> grid = new Grid<>(Contact.class);
+    TextField filterText = new TextField();
+
+    private ContactService contactService;
+
+    public MainView(ContactService contactService) {
+        this.contactService = contactService;
+        addClassName("list-view");
+        setSizeFull();
+        configureGrid();
+        configerFilter();
+
+        add(filterText, grid);
+        updateList();
+    }
+
+    private void configureGrid() {
+        grid.addClassName("contact-grid");
+        grid.setSizeFull();
+        grid.removeColumnByKey("company");
+        grid.setColumns("firstName", "lastName", "email", "status");
+        grid.addColumn(contact -> {
+            Company company = contact.getCompany();
+            return company == null ? "-" : company.getName();
+        }).setHeader("Company");
+
+        grid.getColumns().forEach(col -> col.setAutoWidth(true));
+    }
+
+    private void configerFilter() {
+        filterText.setPlaceholder("Filter by name...");
+        filterText.setClearButtonVisible(true);
+        filterText.setValueChangeMode(ValueChangeMode.LAZY);
+        filterText.addValueChangeListener(e -> updateList());
+    }
+
+    private void updateList() {
+        grid.setItems(contactService.findAll(filterText.getValue()));
+    }
+}
